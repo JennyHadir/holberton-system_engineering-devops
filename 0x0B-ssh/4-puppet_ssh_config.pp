f1c9f3b9dc3c1e58bacc8f35de378da0fbbf9d60
JennyHadir <hadirjenny@hotmail.com>
2021-04-04 22:06:19 +0100

    [ADD]:init add

---
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+#ssh config using puppet
+file_line { 'Turn off passwd auth':
+    path  => '/etc/ssh/ssh_config',
+    line  => '     PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+    path  => '/etc/ssh/ssh_config',
+    line  => '     IdentityFile ~/.ssh/holberton',
+}
