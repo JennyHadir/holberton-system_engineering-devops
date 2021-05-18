a3ddfcbd4c90d86b1c54a30642f926e6697f93f3
JennyHadir <hadirjenny@hotmail.com>
2021-05-18 13:27:07 +0100

    ADD

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,11 @@ def number_of_subscribers(subreddit):
     User = {'User-Agent': 'JennyHadir'}
     r = requests.get('https://www.reddit.com/r/{}/about.json'.
                      format(subreddit),
-                     headers=User).json()
-    if req.status_code == 200:
-        data = r.get('data')
-        subscribers = data.get('subscribers')
-        if data is not None and subscribers is not None:
-            return subscribers
+                     headers=User,
+                     allow_redirects=False).json()
+
+    data = r.get('data')
+    subscribers = data.get('subscribers')
+    if data is not None and subscribers is not None:
+        return subscribers
     return 0
