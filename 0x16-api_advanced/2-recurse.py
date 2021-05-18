5cf1f48dada8a7935c80e5b2332a91bfa29591a5
JennyHadir <hadirjenny@hotmail.com>
2021-05-18 23:29:19 +0100

    ADD

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,21 +6,20 @@ import requests
 def recurse(subreddit, hot_list=[], after=''):
     """ Function to return 10 top posts of subreddit """
     User = {'User-Agent': 'JennyHadir'}
-    r = requests.get('https://www.reddit.com/r/{}/hot.json?after='.
+    r = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'.
                      format(subreddit, after),
                      headers=User,
-                     allow_redirects=False,
-                     params={'limit': 10}).json()
+                     allow_redirects=False).json()
     data = r.get('data')
     after = data.get('after')
     posts = data.get('children')
-    if data is not None and posts is not None:
+    try:
         for post in posts:
             title = post.get('data').get('title')
             hot_list.append(title)
         if after is None:
-            return hot_list
+            return len(hot_list)
         else:
             recurse(subreddit, hot_list, after)
-    else:
-        print('None')
+    except Exception:
+        return None
